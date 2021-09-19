350b0912165fd5d8f5d85994951d3b7416f52bd8
su <liang-shan@outlook.com>
2021-09-19 16:44:02 +0800

    删除其余

--- src/main/java/com/ls/validator/ivalidators/IValidator.java
+++
@@ -1,9 +0,0 @@
-package com.ls.validator.ivalidators;
-
-import java.lang.annotation.Annotation;
-import java.util.Optional;
-
-public interface IValidator {
-
-    Optional<String> apply(Object value, Annotation annotation) throws Exception;
-}
